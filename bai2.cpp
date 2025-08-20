a450f85a60731a691f4dbb459e25051445436e07
luuhuy28 <luuduchuy28@gmail.com>
2025-08-20 22:03:14 +0700

    Add files via upload

---
+++ bai2.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	char ten[10];
+	int tuoi;
+	cin >> ten >> tuoi;
+	cout << "In 15 years, age of " << ten << " will be " << tuoi;
+	return 0;
+}
